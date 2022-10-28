c4e0aa6146fdc1d22ae5b50987ef6c699f973b56
Seiya Nuta <nuta@seiya.me>
2022-10-28 20:57:41 +0900

    Enable analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.scss'
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <>
+        <Component {...pageProps} />
+        <Analytics />
+    </>
 }
 
 export default App
