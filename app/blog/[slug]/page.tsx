f71a913da36792eb5f463b3eb43121991f181729
Seiya Nuta <nuta@seiya.me>
2023-03-25 22:39:03 +0900

    Update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -28,12 +28,14 @@ export default function Post({ params }: { params: { slug: string } }) {
           </Link>
         </div>
         <div className="mb-6 text-center">
-          <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
-          <time dateTime={post.date} className="text-sm text-slate-600">
+          <h1 className="mb-1 text-3xl font-bold dark:text-slate-100">{post.title}</h1>
+          <time dateTime={post.date} className="text-sm text-slate-600 dark:text-slate-400">
             {format(parseISO(post.date), "LLLL d, yyyy")}
           </time>
         </div>
-        <MDXContent components={MDXComponents} />
+        <div className="dark:text-slate-100">
+          <MDXContent components={MDXComponents} />
+        </div>
       </article>
     </>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body className="dark:bg-slate-800">{children}</body>
     </html>
   );
 }
