fb43b4be2e324a25f6cd8483594e93b8184a3766
Seiya Nuta <nuta@seiya.me>
2023-03-26 15:14:44 +0900

    Update

--- components/Email.tsx
+++ components/Email.tsx
@@ -2,6 +2,14 @@
 
 import { useEffect, useState } from 'react';
 
+function reverse(text) {
+    let reversed = '';
+    for (let i = text.length - 1; i >= 0; i--) {
+        reversed += text[i];
+    }
+    return reversed;
+}
+
 export default function Email() {
     const chars = [
         196, 223, 222, 203,
@@ -9,12 +17,31 @@ export default function Email() {
         211, 203, 132, 199,
         207
     ];
-    const [text, setText] = useState('-');
-    useEffect(() => {
-        setText(chars.map(c => String.fromCharCode(c ^ 0xaa)).join(''));
-    })
 
+    const getEmail = () => {
+        return chars.map(c => String.fromCharCode(c ^ 0xaa)).join('');
+    }
+
+    const [text, setText] = useState(reverse(getEmail()));
+
+    const [styles, setStyles] = useState({
+        "unicode-bidi": "bidi-override",
+        direction: "rtl"
+    });
+
+    const reveal = () => {
+            setText(getEmail());
+            setStyles({});
+    }
+
+    const [user, domain] = text.split('@');
+    let atmark = '';
+    if (domain) {
+        atmark = '@';
+    }
     return (
-        <span>{text}</span>
+        <span style={styles} onMouseOver={reveal}>
+            {user}<span style={{display: "none"}}>invisible</span>{atmark}{domain}
+        </span>
     )
 }
