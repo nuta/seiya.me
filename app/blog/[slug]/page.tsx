73c110d5ce511d841f5d7dba8eb715599a73b272
Seiya Nuta <nuta@seiya.me>
2023-04-02 05:22:52 +0900

    Update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -49,31 +49,29 @@ export default function Post({ params }: { params: { slug: string } }) {
   const MDXContent = useMDXComponent(post.body.code);
 
   return (
-    <>
-      <article className="prose mx-auto max-w-2xl py-16">
-        <div className="mb-6 text-center">
-          <Link
-            href="/"
-            className="text-center text-sm font-bold uppercase text-blue-700"
-          >
-            Home
-          </Link>
-        </div>
-        <div className="mb-6 text-center">
-          <h1 className="mb-1 text-3xl font-bold dark:text-slate-200">
-            {post.title}
-          </h1>
-          <time
-            dateTime={post.date}
-            className="text-sm text-slate-600 dark:text-slate-400"
-          >
-            {format(parseISO(post.date), "LLLL d, yyyy")}
-          </time>
-        </div>
-        <div className="dark:text-slate-100">
-          <MDXContent components={MDXComponents} />
-        </div>
-      </article>
-    </>
+    <article className="prose dark:prose-invert prose-neutral mx-auto max-w-2xl py-16">
+      <div className="mb-6 text-center">
+        <Link
+          href="/"
+          className="text-center text-sm font-bold uppercase text-blue-700"
+        >
+          Home
+        </Link>
+      </div>
+      <div className="mb-6 text-center">
+        <h1 className="mb-1 text-3xl font-bold dark:text-slate-200">
+          {post.title}
+        </h1>
+        <time
+          dateTime={post.date}
+          className="text-sm text-slate-600 dark:text-slate-400"
+        >
+          {format(parseISO(post.date), "LLLL d, yyyy")}
+        </time>
+      </div>
+      <div>
+        <MDXContent components={MDXComponents} />
+      </div>
+    </article>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,46 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: "ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji",
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            fontSize: '1.05rem',
+            'blockquote p:first-of-type::before': { content: 'none' },
+            'blockquote p:first-of-type::after': { content: 'none' },
+            'code::before': { content: 'none' },
+            'code::after': { content: 'none' },
+            a: {
+              color: theme('colors.sky[500]'),
+              textDecoration: 'none',
+              fontWeight: '500',
+            },
+            "a:hover": {
+              color: theme('colors.sky[700]'),
+              textDecoration: 'underline',
+              fontWeight: '500',
+            },
+            'a.anchor': {
+              color: 'var(--tw-prose-title)',
+              textDecoration: 'none',
+            },
+            "pre, pre[class*=language-]": {
+              background: "#272b33",
+              color: "#aab1bf",
+              margin: '0 -120px !important',
+            },
+            "blockquote pre": {
+              margin: '0 0px !important',
+            },
+            "span.comment": {
+              fontWeight: "bold",
+              color: "#8b92a0 !important",
+            }
+          },
+        },
+      }),
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
