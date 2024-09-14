2bff2b38dfbafe7cb5f6e7627579a2a9302fca14
Seiya Nuta <nuta@seiya.me>
2024-09-14 19:41:14 +0900

    Update

--- components/BottomBar.tsx
+++
@@ -1,9 +0,0 @@
-export default function BottomBar() {
-    return (
-        <div className="text-sm text-center text-neutral-400">
-            —<br />
-            written by Seiya Nuta<br />
-            CC BY 4.0
-        </div>
-    );
-}
