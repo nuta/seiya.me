fd613fafc8ca4f94b6700f51a911fa4cfe6d5709
Seiya Nuta <nuta@seiya.me>
2025-05-06 20:50:57 +0900

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import Image from "next/image";
+import MyPicture from "./me.jpg";
 export default function Home() {
   return (
-    <div>
+    <div className="font-['Times']">
+      <Image src={MyPicture} alt="selfie" />
+      <h1 className="text-2xl font-bold">Seiya Nuta</h1>
+      <p>
+      </p>
     </div>
   );
 }
