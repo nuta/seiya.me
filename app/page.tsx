39ab0fafa42db4545d1d710b3b3b34878893d0a9
Seiya Nuta <nuta@seiya.me>
2025-05-06 21:05:38 +0900

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,55 @@
 import Image from "next/image";
 import MyPicture from "./me.jpg";
 import Link from "next/link";
+import "./home.css";
+
 export default function Home() {
   return (
     <div className="font-['Times']">
-      <div className="flex flex-row items-center">
-        <div className="">
+      <header className="flex flex-row items-center">
+        <div>
           <Image src={MyPicture} alt="selfie" className="rounded max-w-24" />
         </div>
-        <div className="ml-5 flex flex-col justify-between">
+        <div className="ml-5 py-1 flex flex-col justify-between self-stretch">
           <div>
             <h1 className="text-2xl font-bold">Seiya Nuta</h1>
             <p>
               An operating system kernel enthusiast.
             </p>
           </div>
-          <nav className="flex flex-row gap-2">
+          <nav className="flex flex-row gap-4">
             <Link href="https://github.com/nuta" prefetch={false}>GitHub</Link>
             <Link href="https://twitter.com/seiyanuta" prefetch={false}>Twitter</Link>
             <Link href="https://www.linkedin.com/in/seiyanuta/" prefetch={false}>LinkedIn</Link>
           </nav>
         </div>
-      </div>
-      <p>
-      </p>
-    </div>
+      </header>
+      <main className="my-12">
+        <section>
+          <h2 className="text-2xl font-bold">Projects</h2>
+          <ul>
+            <li>
+              <Link href="https://github.com/starina-os/starina" prefetch={false}>Starina operating system</Link>
+            </li>
+          </ul>
+        </section>
+
+        <section>
+          <h2 className="text-2xl font-bold">Publications</h2>
+          <ul>
+            <li>
+            </li>
+          </ul>
+        </section>
+
+        <section>
+          <h2 className="text-2xl font-bold">Posts</h2>
+          <ul>
+            <li>
+            </li>
+          </ul>
+        </section>
+      </main>
+   </div>
   );
 }
