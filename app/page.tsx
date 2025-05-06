838442253ef1bb070006c523352d0c57ad36f4f9
Seiya Nuta <nuta@seiya.me>
2025-05-06 20:57:45 +0900

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
 import MyPicture from "./me.jpg";
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="font-['Times']">
-      <Image src={MyPicture} alt="selfie" />
-      <h1 className="text-2xl font-bold">Seiya Nuta</h1>
+      <div className="flex flex-row items-center">
+        <div className="">
+          <Image src={MyPicture} alt="selfie" className="rounded max-w-24" />
+        </div>
+        <div className="ml-5 flex flex-col justify-between">
+          <div>
+            <h1 className="text-2xl font-bold">Seiya Nuta</h1>
+            <p>
+              An operating system kernel enthusiast.
+            </p>
+          </div>
+          <nav className="flex flex-row gap-2">
+            <Link href="https://github.com/nuta" prefetch={false}>GitHub</Link>
+            <Link href="https://twitter.com/seiyanuta" prefetch={false}>Twitter</Link>
+            <Link href="https://www.linkedin.com/in/seiyanuta/" prefetch={false}>LinkedIn</Link>
+          </nav>
+        </div>
+      </div>
       <p>
       </p>
     </div>
