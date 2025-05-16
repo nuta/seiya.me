e9edb9718cf727f1ea5f5e1ad83607747d132e9b
Seiya Nuta <nuta@seiya.me>
2025-05-16 19:35:43 +0900

    Update

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,27 +1,31 @@
 import Link from "next/link"
 
 const navItems = [
-    {
-        label: "home",
-        href: "/",
-    },
-    {
-        label: "github",
-        href: "https://github.com/nuta",
-    },
-    
+  {
+    label: "about",
+    href: "/",
+  },
+  {
+    label: "blog",
+    href: "/#posts",
+  },
+  {
+    label: "github",
+    href: "https://github.com/nuta",
+  },
+
 ]
 
 export default function Navbar() {
-    return (
-        <nav
-          className="flex flex-row items-start mb-8"
-        >
-          {navItems.map((item) => (
-            <Link key={item.href} href={item.href} prefetch={false} className="py-1 pr-6">
-              {item.label}
-            </Link>
-          ))}
-        </nav>
-    )
+  return (
+    <nav
+      className="flex flex-row items-start mb-8"
+    >
+      {navItems.map((item) => (
+        <Link key={item.href} href={item.href} prefetch={false} className="py-1 pr-6">
+          {item.label}
+        </Link>
+      ))}
+    </nav>
+  )
 }
