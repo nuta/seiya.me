53c1433c2913e565d4082924d5aa5b1c5f10ca1f
Seiya Nuta <nuta@seiya.me>
2025-05-17 11:32:51 +0900

    Prefetch more

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,14 +4,17 @@ const navItems = [
   {
     label: "about",
     href: "/",
+    prefetch: true,
   },
   {
     label: "blog",
     href: "/blog",
+    prefetch: true,
   },
   {
     label: "github",
     href: "https://github.com/nuta",
+    prefetch: false,
   },
 
 ]
@@ -22,7 +25,7 @@ export default function Navbar() {
       className="flex flex-row items-start mb-8"
     >
       {navItems.map((item) => (
-        <Link key={item.href} href={item.href} prefetch={false} className="py-1 pr-6">
+        <Link key={item.href} href={item.href} prefetch={item.prefetch} className="py-1 pr-6">
           {item.label}
         </Link>
       ))}
